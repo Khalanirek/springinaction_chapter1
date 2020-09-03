22704950261bc55bd67611c9e27ef4a3bef05a18
Piotr Turlinski <piotrturlinski2@gmail.com>
2020-09-03 20:44:40 +0200

    Init commit

---
+++ src/main/java/training/springinaction_chapter1/SpringinactionChapter1Application.java
@@ -0,0 +1,13 @@
+package training.springinaction_chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinactionChapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinactionChapter1Application.class, args);
+	}
+
+}
